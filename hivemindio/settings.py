b1f7912500a0492b271685231dd18e4f0a0cc498
noahpryor <noah@noahpryor.com>
2012-09-05 02:18:10 -0400

    bootstrpa sorta workin

--- hivemindio/settings.py
+++ hivemindio/settings.py
@@ -149,7 +149,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'bootstrapped',
+    'bootstrap_toolkit',
     'south',
     'gunicorn',
     'djcelery',
