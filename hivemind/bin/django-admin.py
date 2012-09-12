c1524b3925faa238cfbe79a2313191d612bb3294
noahpryor <noah@noahpryor.com>
2012-09-11 23:12:50 -0400

    test

---
+++ hivemind/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nsp2115/work/hivemind/hivemind/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- hivemindio/settings.py
+++ hivemindio/settings.py
@@ -149,7 +149,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'bootstrap_toolkit',
     'south',
     'gunicorn',
     'djcelery',
