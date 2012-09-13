149241adcefdd77805e32f0c9e0ba9275f22df40
Patrick McGuire <pjm2119@columbia.edu>
2012-09-12 22:17:11 -0400

    changed up the virtual environment

--- bin/django-admin.py
+++
@@ -1,7 +0,0 @@
-#!/Users/nsp2115/work/hivemind/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==1.5.dev17942','django-admin.py'
-__requires__ = 'Django==1.5.dev17942'
-from pkg_resources import require; require('Django==1.5.dev17942')
-del require
-__file__ = '/Users/nsp2115/work/hivemind/src/django/django/bin/django-admin.py'
-execfile(__file__)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/patrick/Code/hivemind/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
