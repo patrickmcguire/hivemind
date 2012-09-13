89e9e710d54856b6f5f39c81becbdc8331457137
noahpryor <noah@noahpryor.com>
2012-09-12 20:16:49 -0400

    started styling best comments

--- hivemindio/settings.py
+++ hivemindio/settings.py
@@ -33,8 +33,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'hivemindio',                      # Or path to database file if using sqlite3.
-        'USER': 'hivemindio',                      # Not used with sqlite3.
-        'PASSWORD': 'calculus',                  # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
