c02f26478801fd7ec39837f9ebc80aa4a542bb62
noahpryor <noah@noahpryor.com>
2012-09-17 02:29:51 -0400

    demo page tweaks, color scheme maybe found for graphs?

--- hivemindio/urls.py
+++ hivemindio/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns(
     url(r'^bwog/versus/$', 'main.views.versus'),
     url(r'^$', 'main.views.index'),
     url(r'^bwog/predictions/$', 'main.views.predictions'),
+    url(r'^privacy.txt', 'django.views.generic.simple.direct_to_template', {'template': 'bwog/privacy.txt'}),
     url(r'^test', 'django.views.generic.simple.direct_to_template', {'template': 'bwog/index2.html'}),
     url(r'^google09e6491b8ce4f523.html', 'django.views.generic.simple.direct_to_template', {'template': 'bwog/google09e6491b8ce4f523.html'})
 )
